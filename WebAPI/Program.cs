32951149d180b93078acabe5df0c7439c6028535
nese <nesekrmn02@gmail.com>
2024-07-11 08:59:45 +0300

    ders11 odev yapıldı

---
+++ WebAPI/Program.cs
@@ -0,0 +1,39 @@
+using ConsoleApp1.Business.Abstract;
+using ConsoleApp1.Business.Concrete;
+using ConsoleApp1.DataAccess.Abstract;
+using ConsoleApp1.DataAccess.Concrete;
+using RecapProjectCar.Business.Abstract;
+using RecapProjectCar.Business.Concrete;
+using RecapProjectCar.DataAccess.Abstract;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddSingleton<ICarService, CarManager>();
+builder.Services.AddSingleton<ICarDal , EfCarDal>();
+builder.Services.AddSingleton<IBrandService, BrandManager>();
+builder.Services.AddSingleton<IBrandDal, EfBrandDal>();
+builder.Services.AddSingleton<IColorService, ColorManager>();
+builder.Services.AddSingleton<IColorDal, EfColorDal>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
